134303eed6fa19021f2751a4b23241e158f5a17a
ngugiii <108797418+ngugiii@users.noreply.github.com>
2022-11-02 09:20:00 +0300

    updated the products page

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,35 +1,54 @@
 async function loadfeaturedProducts(featuredProductsCallback) {
-    fetch("/public/data/featuredProducts.json",{headers: { 'content-type': 'application/json'}})
-    .then(function(data){
-        return data.json();
-    })
-    .then(function(data){
-        featuredProductsCallback(data.featuredProducts);
+    var data = await fetch("/public/data/featuredProducts.json",{headers: { 'content-type': 'application/json'}})
+    var json = await data.json();
 
-    });
+  featuredProductsCallback(json.featuredProducts);
+
 }
 
+//     .then(function(data){
+//         return data.json();
+//     })
+//     .then(function(data){
+//         featuredProductsCallback(data.featuredProducts);
+
+//     });
+// }
 async function loadlatestProducts(latestProductsCallback) {
+    var data = await fetch("/public/data/latestProducts.json",{headers: { 'content-type': 'application/json'}})
+    var json = await data.json();
 
-    fetch("/public/data/latestProducts.json",{headers: { 'content-type': 'application/json'}})
-    .then(function(data){
-        return data.json();
-    })
-    .then(function(data){
-        latestProductsCallback(data.latestProducts);
+  latestProductsCallback(json.latestProducts);
 
-    });
 }
-async function loadAllProducts(AllProductsCallback) {
-    fetch("/public/data/AllProducts.json",{headers: { 'content-type': 'application/json'}})
-    .then(function(data){
-        return data.json();
-    })
-    .then(function(data){
-        AllProductsCallback(data.AllProducts);
+//  function loadlatestProducts(latestProductsCallback) {
 
-    });
+//     fetch("/public/data/latestProducts.json",{headers: { 'content-type': 'application/json'}})
+//     .then(function(data){
+//         return data.json();
+//     })
+//     .then(function(data){
+//         latestProductsCallback(data.latestProducts);
+
+//     });
+// }
+
+async function loadAllProducts(AllProductsCallback){
+    var data = await fetch("/public/data/AllProducts.json",{headers: { 'content-type': 'application/json'}})
+    var json = await data.json();
+
+    AllProductsCallback(json.AllProducts);
 }
+// function loadAllProducts(AllProductsCallback) {
+//     fetch("/public/data/AllProducts.json",{headers: { 'content-type': 'application/json'}})
+//     .then(function(data){
+//         return data.json();
+//     })
+//     .then(function(data){
+//         AllProductsCallback(data.AllProducts);
+
+//     });
+// }
 function renderfeaturedProducts(listId, featuredProducts){
 
     const listContainer=document.getElementById(listId);
@@ -38,7 +57,7 @@ function renderfeaturedProducts(listId, featuredProducts){
     for (var index=0; index < featuredProducts.length; index++){
         const product = featuredProducts[index];
 
-        const listItem = "<li><img src=" + product.picture + "><div>" + product.name + "<div></li>";
+        const listItem = "<li><img src=" + product.picture + "><div>" + product.name + "<div><div> Ksh "+product.price +"<div></li>";
         innerHTML+=listItem;
     }
 
@@ -58,7 +77,7 @@ function renderlatestProducts(listId, latestProducts){
     for (var index=0; index < latestProducts.length; index++){
         const product = latestProducts[index];
 
-        const listItem = "<li><img src=" + product.picture + "><div>" + product.name + "<div></li>";
+        const listItem = "<li><img src=" + product.picture + "><div>" + product.name + "<div><div> Ksh "+product.price +"<div></li>";
         innerHTML+=listItem;
     }
 
@@ -78,7 +97,7 @@ function renderAllProducts(listId, AllProducts){
     for (var index=0; index < AllProducts.length; index++){
         const product = AllProducts[index];
 
-        const listItem = "<li><img src=" + product.picture + "><div>" + product.name + "<div></li>";
+        const listItem = "<li><img src=" + product.picture + "><div>" + product.name + "<div><div> Ksh "+product.price +"<div></li>";
         innerHTML+=listItem;
     }
 
